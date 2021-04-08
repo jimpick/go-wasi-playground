4bc5e2a51bc788749af8b2ff7a974ca6da6f2fcd
Jim Pick <jim@jimpick.com>
2021-04-08 16:38:48 -0700

    Use pako and gzip wasm bundle

--- wasmer-js-browser-glif-read-only-demo/index.js
+++ wasmer-js-browser-glif-read-only-demo/index.js
@@ -1,13 +1,54 @@
 import WasmTerminal, { fetchCommandFromWAPM } from '@jimpick/wasm-terminal'
 import { lowerI64Imports } from '@wasmer/wasm-transformer'
 import { BrowserProvider } from './browser-provider'
+import pako from 'pako'
+
+const wasmSize = 6037998
+
+async function download (url, defaultLength, status) {
+  // https://dev.to/samthor/progress-indicator-with-fetch-1loo
+  const response = await fetch(url)
+  let length = response.headers.get('Content-Length')
+  if (!length) {
+    length = defaultLength
+    // something was wrong with response, just give up
+    // return await response.arrayBuffer()
+  }
+  const array = new Uint8Array(length)
+  let at = 0 // to index into the array
+  const reader = response.body.getReader()
+  for (;;) {
+    const { done, value } = await reader.read()
+    if (done) {
+      break
+    }
+    status.textContent = `Fetching data... ${at} of ${length} bytes`
+    array.set(value, at)
+    at += value.length
+  }
+  return array
+}
+
+async function preloadLotusWasm () {
+  const status = document.getElementById('status')
+  status.textContent = 'Fetching data... (6MB compressed)'
+  const compressed = await download('/lotus.wasm.gz', wasmSize, status)
+  status.textContent = `Uncompressing data... (compressed size: ${compressed.byteLength} bytes)`
+  const buffer = pako.ungzip(compressed)
+  const size = +buffer.buffer.byteLength
+  status.textContent = `WASM fetched: ${size} bytes`
+  return buffer
+}
+const preloadedLotusPromise = preloadLotusWasm()
 
 // Let's write handler for the fetchCommand property of the WasmTerminal Config.
+let cachedLotus
 const fetchCommandHandler = async ({ args }) => {
   console.log('Jim fetchCommand', args)
   let commandName = args[0]
   // Let's return a "CallbackCommand" if our command matches a special name
   if (commandName === 'lotus') {
+    if (cachedLotus) return cachedLotus
     const options = {}
     const lotusUrl = 'https://api.node.glif.io/rpc/v0'
     const browserProvider = new BrowserProvider(lotusUrl, options)
@@ -32,8 +73,8 @@ const fetchCommandHandler = async ({ args }) => {
       waitForResult()
     }
 
-    let response = await fetch('/lotus.wasm')
-    let wasmBinary = new Uint8Array(await response.arrayBuffer())
+    let wasmBinary = await preloadedLotusPromise
+    cachedLotus = wasmBinary
     return wasmBinary
   }
 
