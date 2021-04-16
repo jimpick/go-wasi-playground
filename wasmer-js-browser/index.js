1b438fe9ab478a0d440589483f4f9191b336f968
Jim Pick <jim@jimpick.com>
2021-04-16 08:51:08 -0700

    Get CLI `lotus send` working with Ganache

--- wasmer-js-browser/index.js
+++ wasmer-js-browser/index.js
@@ -121,9 +121,9 @@ const fetchCommandHandler = async ({ args }) => {
   }
   if (commandName === 'lotus') {
     const options = {}
-    // const wsUrl = 'ws://127.0.0.1:7777/rpc/v0'
-    const wsUrl = 'wss://lotus.jimpick.com/mainnet_api/0/node/rpc/v0'
-    const ganache = wsUrl.match(/127.0.0.1/)
+    const wsUrl = 'ws://127.0.0.1:7777/rpc/v0'
+    // const wsUrl = 'wss://lotus.jimpick.com/mainnet_api/0/node/rpc/v0'
+    const ganache = !!wsUrl.match(/127\.0\.0\.1/)
     if (!ganache) {
       options.token = localStorage.getItem('token')
     }
@@ -139,12 +139,18 @@ const fetchCommandHandler = async ({ args }) => {
             if (request.method === 'Filecoin.WalletBalance') {
               request.params[0] = request.params[0].replace(/^f/, 't')
             }
+            if (request.method === 'Filecoin.MpoolPushMessage') {
+              request.params[0].From = request.params[0].From.replace(/^f/, 't')
+              request.params[0].To = request.params[0].To.replace(/^f/, 't')
+            }
           }
           const result = await browserProvider.sendWs(request)
-          console.log('Jim result', JSON.stringify(result))
-          // Ganache fixups
+          console.log('Jim result', ganache, JSON.stringify(result))
           if (ganache) {
-            if (request.method === 'Filecoin.ChainHead') {
+            if (
+              request.method === 'Filecoin.ChainHead' ||
+              request.method === 'Filecoin.ChainGetTipSet'
+            ) {
               for (const block of result.result.Blocks) {
                 block.Timestamp = Math.floor(block.Timestamp)
               }
@@ -153,9 +159,16 @@ const fetchCommandHandler = async ({ args }) => {
               result.result.BlockDelay = Number(result.result.BlockDelay)
             }
           }
+          console.log('Jim fixed result', JSON.stringify(result))
           responseHandler(JSON.stringify(result))
         } catch (e) {
           console.error('JSON-RPC error', e.message)
+          const errorResult = {
+            jsonrpc: "2.0",
+            id: request.id,
+            error: { message: e.message }
+          }
+          responseHandler(JSON.stringify(errorResult))
         }
       }
       waitForResult()
@@ -181,10 +194,10 @@ const wasmTerminal = new WasmTerminal({
 })
 
 // Let's print out our initial message
-wasmTerminal.print("Type 'demo' to run demo.wasm\n")
-wasmTerminal.print("Type 'demo-wasi' to run demo-wasi.wasm\n")
-wasmTerminal.print("Type 'demo-go' to run demo-go.wasm\n")
-wasmTerminal.print("Type 'api-client' to run api-client.wasm\n")
+// wasmTerminal.print("Type 'demo' to run demo.wasm\n")
+// wasmTerminal.print("Type 'demo-wasi' to run demo-wasi.wasm\n")
+// wasmTerminal.print("Type 'demo-go' to run demo-go.wasm\n")
+// wasmTerminal.print("Type 'api-client' to run api-client.wasm\n")
 wasmTerminal.print("Type 'lotus' to run lotus.wasm\n")
 
 // Let's bind our Wasm terminal to it's container
